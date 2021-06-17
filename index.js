9bfd2c83e991927f4eda4218a152adc0fc379bf7
hirsiz <zakeria.hirsi1@gmail.com>
2021-06-18 00:58:45 +0100

    making changes from github

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Shaqo Yeelo'),
+        body: JSON.stringify('Shaqo Yeelo updateed from git'),
     };
     return response;
 };
