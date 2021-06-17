f8ba2f70f282644766b992620af0196ae5322b78
hirsiz <zakeria.hirsi1@gmail.com>
2021-06-18 00:44:37 +0100

    adding lambda functionality

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Shaqo Yeelo'),
+    };
+    return response;
+};
