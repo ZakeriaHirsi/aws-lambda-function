d4b3b3773e3f7c8e2f19bc45554a69a8c1c3c5a6
hirsiz <zakeria.hirsi1@gmail.com>
2021-06-20 17:55:48 +0100

    updating index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Shaqo Yeelo updated from git'),
+        body: JSON.stringify('Shaqo Yeelo removed from git'),
     };
     return response;
 };
